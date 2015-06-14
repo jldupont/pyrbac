550a10448b6ca23c8ad1b580396dfc462f00ba8f
github@jldupont.com <github@jldupont.com>
2015-06-14 14:15:50 -0400

    setup.py

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 pyrbac
 ======
 
-Role Based Access Control in Python
+Role Based Access Control in Python loosely based on the NIST model (see http://csrc.nist.gov/groups/SNS/rbac/ ).
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""
+    Jean-Lou Dupont's Python RBAC (Role Based Access Control)
+    
+    Created on 2015-06-14
+    @author: jldupont
+"""
+__author__  ="Jean-Lou Dupont"
+__version__ ="0.1"
+
+DESC="""
+RBAC (Role Based Access Control) library loosely based on NIST model (see http://csrc.nist.gov/groups/SNS/rbac/ ).
+"""
+
+
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(name=         'pyrbac',
+      version=      __version__,
+      description=  'RBAC (Role Based Access Control) library',
+      author=       __author__,
+      author_email= 'jl@jldupont.com',
+      url=          'https://github.com/jldupont/pyrbac',
+      package_dir=  {'': "src",},
+      packages=     find_packages("src"),
+      scripts=      [
+                     ]
+      ,zip_safe=True
+      ,long_description=DESC
+      ,install_requires=[
+                         ]
+      )
+
+#############################################
+
+f=open("latest", "w")
+f.write(str(__version__)+"\n")
+f.close()
+
